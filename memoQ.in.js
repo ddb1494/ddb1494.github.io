d82a059e92efb908bb7affbbedd56786441cd6dd
aonuncom <35029624+ddb1494@users.noreply.github.com>
2018-07-16 09:07:51 +0900

    Update memoQ.in.js

--- memoQ.in.js
+++ memoQ.in.js
@@ -1,41 +1,3 @@
-// DOMAttrModified
-// DOMAttributeNameChanged
-// DOMCharacterDataModified
-// DOMElementNameChanged
-// DOMNodeInserted
-// DOMNodeInsertedIntoDocument
-// DOMNodeRemoved
-// DOMNodeRemovedFromDocument
-// DOMSubtreeModified
-/*
-<tbody id="gridTableBody">
-<tr  data-id="0"  class style="display:table-row">
-    <td class="order">1.</td>
-    <td class="original-segment-grid">
-        <div class="editor-cell-container" style="max-height:534px">
-            <div class="editor-cell">
-                <div class="asset-container"></div>
-                <div class="content-container-wrapper">
-                    <span class="content-container">
-                        <span class=" editor-char  ">검투사의 가죽 장화</span></span></div></div>
-    <td class="translated-segment-grid">
-        <div class="editor-cell-container focused" style="max-height: 746px;">
-            <div class="editor-cell">
-                <div class="asset-container">
-                    <div class="editor-cursor hidden" style="width: 0px; height: 15px; left: 0px; top: 0px;"></div></div>
-                    <div class="content-container-wrapper">
-                        <!-- 有两种可能性, 空的没有<span class="editor-char" /> -->
-                        <span class="content-container"></span></div></div></div>
-                        <span class="content-container"><span class=" editor-char  ">112341</span></span>
-
-
-页面侦听wheel事件
-
-如果array还有有效内容, 
-    填充(#gridTableBody tr[data-id=?] .translated-segment-grid span.content-container)
-    下的span.editor-char
-*/
-
 (function() {
 
 let str, map, target, div, msg, ok, ng, ta, ui, input;
@@ -44,8 +6,8 @@ input=document.querySelector('#editorHiddenInput');
 
 ui = $(`<div style="position:fixed; top:0; right:10px; width:40%; bottom:10px; border:2px solid blue; padding:24px; background:#0007; z-index: 99999;">
 <h5 class="message" style="background:#fffc; font-weight:bold;">memoQ 위한 붙여넣는 기능입니다.</h5>
-<button class="ok" style="text-align:center;background:#ff0">확인</button>
-<button class="ng" style="text-align:center;background:#ff0">취소</button>
+<button class="ok" style="text-align:center;background:#ff0;width:100px;height:30px;">확인</button>
+<button class="ng" style="text-align:center;background:#ff0;width:100px;height:30px;">취소</button>
 <textarea style="width:100%; height:80%;"></textarea>
 </div>`).appendTo('body');
 
@@ -70,11 +32,19 @@ ng = ng.on("click", function() {
 });
 
 function doFill(e) {
-    if(e.keyCode===40||e.keyCode===38){
+    let keyCode=e.keyCode;
+    let fName='';
+    if(keyCode===40||keyCode===38){
+        // previousElementSibling
+        // nextElementSibling
+
+        fName= (keyCode===40?'next':'previous')+'ElementSibling';
+
         msg.textContent='남은 수: '+map.size+'개.';
         if(map.size) {
-            setTimeout(()=>{
+            // setTimeout(()=>{
                 let active=document.querySelector('#gridTableBody tr.active');
+                active=active[fName]
                 if(active){
                     let id=active.getAttribute('data-id');
                     if(map.has(id)){
@@ -89,7 +59,7 @@ function doFill(e) {
                     }     
                 }
                 console.log(active);
-            });
+            // });
         }else{
             msg.text("입력 완료.").css({
                 background: "rgba(0,255,0,0.2)"
@@ -108,36 +78,5 @@ function stringToMap(str){
     });
     return map;
 }
-
-// function fill(id, value) {
-//     let t,c,i;
-//     t=document.querySelector('#gridTableBody tr[data-id="'+id+'"] .translated-segment-grid span.content-container');
-//     c=t.querySelector('span.editor-char');
-//     if(c===null) {
-//         c=document.createElement('span');
-//         x.classList('editor-char');
-//         t.appendChild(c);
-//     }
-//     c.textContent=value;
-//     c.style.background='#ccc';
-    
-//     let e=new Event('keyup');
-//     e.keyCode=13;
-//     e.ctrlKey=true;
-//     e.shiftKey=false;
-//     e.altKey=false;
-//     e.metaKey=false;
-//     t.dispatchEvent(e);
-// }
-
-
-// $(window).on('keydow', e=>console.log(e.type))
-
-// function getRows() {
-//     let rows;
-//     rows=document.querySelectorAll('#gridTableBody tr[data-id]');
-//     return rows;
-// }
-
 
 })();
