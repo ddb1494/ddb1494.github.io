4f3f309a7cc048d8ee2db677a857d238ad4b0574
aonuncom <35029624+ddb1494@users.noreply.github.com>
2018-07-18 10:50:54 +0900

    Update memoQ.in.js

--- memoQ.in.js
+++ memoQ.in.js
@@ -1,7 +1,7 @@
 // 免费计数器
 // http://www.reliablecounter.com/signup.php?digit=style/plain/5/
 $('<img>').appendTo('body')
-   .attr('src','https://www.reliablecounter.com/count.php?page=https://github.com/ddb1494/ddb1494.github.io/blob/master/memoQ.in.js&digit=style/plain/5/&reloads=0')
+   .attr('src','https://www.reliablecounter.com/count.php?page=https://github.com/ddb1494/ddb1494.github.io/blob/master/memoQ.in.js&digit=style/plain/5/&reloads=0'+'&'+Date.now())
    .css({position:'fixed',right:0,top:0,zIndex:999})
 
 // DOMAttrModified
