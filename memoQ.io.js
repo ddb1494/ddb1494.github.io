3718c1e71391e14bf1da2fa0525c0ba5b2855bf4
aonuncom <35029624+ddb1494@users.noreply.github.com>
2018-07-23 16:43:22 +0900

    Add files via upload

--- memoQ.io.js
+++ memoQ.io.js
@@ -1,94 +1,345 @@
-// 免费计数器
+// {
+//    console.log('loaded',this);
 
-{
-   let rows=[
-       ['[[진] 빛나는 대영주의 부츠 상자','[[真]闪耀的大领主之靴宝箱'],
-       ['[[진] 빛나는 대영주의 코트 상자','[[真]闪耀的大领主大衣宝箱'],
-   ];
+   // function createArrayRange(start,end){
+   //      if(end===undefined) {
+   //          end=start;
+   //          start=0;
+   //      }
+   //      let res=[];
+   //      while(start<=end){
+   //          res.push(start++);
+   //      }
+   //      return res;
+   //  }
 
-   function createArrayRange(start,end){
-        if(end===undefined) {
-            end=start;
-            start=0;
-        }
-        let res=[];
-        while(start<=end){
-            res.push(start++);
-        }
-        return res;
-    }
+//    function getWebContent(id, start, end, callback){
+//         let argLen=arguments.length;
+//         if(argLen===3) {
+//             callback=end;
+//             end=start;
+//             start=0
+//         }else if(argLen!==4){
+//             throw new Error('arguments length.');
+//         }
 
-   function saveWebRows(id, rows, callback){
-        rows=rows.map((e,i)=>{
-            let o = {
-                DocInstanceId:id,
-                id:i,
-                comments:[],
-                locked:false,
-                rangeForCorrectedLQA:null,
-                // sourceSegmentHtml:e[0],
-                // sourceSegmentChanges:[],
-                // targetSegmentChanges:[],
-                // targetSegmentHtml:e[1],
-                translationState:1,
-                warnings:[],
-                webLQAErrors:[]
-            };
+//         let url=MQ.getAppRootUrl()+'api/TranslationService/GetWebContent';
+//         let data={"DocInstanceId":id,
+//         "RowIndicies":createArrayRange(start,end)}
 
-            let type=typeof e;
-            if(type==='string'){
-                o.targetSegmentHtml=e;
-            }else if(Array.isArray(e)){
-                if(e.length>1) {
-                    o.sourceSegmentHtml=e[0];
-                    o.targetSegmentHtml=e[1];
-                }else{
-                    o.targetSegmentHtml=e[0];
-                }
+//         let headers={
+//             'accept':'*/*',
+//             'content-type':'application/json; charset=UTF-8',
+//             // 'x-xsrf-token':document.cookie.match(/X-XSRF-TOKEN=(.+);?/)[1]
+//             'x-xsrf-token':MQ.getCookieValue('X-XSRF-TOKEN')
+//         };
+
+//         function success(data){
+//             if(typeof callback==='function') {
+//                 callback(data);
+//             }else{
+//                 console.log(data);
+//             }
+//         }
+
+//         $.ajax({
+//           type: "POST",
+//           data: JSON.stringify(data),
+//           // dataType: '*/*',
+//           headers, url, success
+//         });
+//         // 实例：
+//         // f(1,5)  // [1,2,3,4,5]
+//         // f(5,7)  // [5,6,7]
+//         // f(3)    // [0,1,2,3]
+
+//     }
+
+//     let id=WebTrans.Doc.docInstanceId;
+//     if(id){
+//         getWebContent(id,0,prompt('count'),function(data){
+//             let success=data.Success, res;
+//             if(success){
+//                 res=data.Value.Rows;
+//                 res=res.map(row=>{
+//                     return row.Row.SourceSegment.EditorString;
+//                 })
+                
+//                 let textarea=$('<textarea>');
+//                 textarea.one('contextmenu',(e)=>{
+//                    textarea.remove();
+//                    e.preventDefault();
+//                 }).appendTo("body")
+//                 .css({position:'fixed',top:0,left:0,zIndex:999,width:'33%',height:'100%',color:'#fff',background:'#333'})
+//                 .val(res.join('\n'))
+//                 .attr('title','Click mouse right button to close.')
+//             }
+//         });
+//     }
+
+    
+
+// }
+
+
+function MemoQ(){
+    if(!(this instanceof MemoQ))new MemoQ();
+
+    this.id           = WebTrans.Doc.docInstanceId;
+    this.length       = WebTrans.Core.numberOfRows;
+    this.divisionGuid = WebTrans.Core.docJobGuid;
+    this.projectId    =  MQ.getQueryString('prj');
+    this.documentId   = MQ.getQueryString('doc');
+    this.root         = MQ.getAppRootUrl();
+}
+MemoQ.prototype.ajax = function (url,data,callback){
+    let headers;
+    url = MQ.getAppRootUrl()+url;
+    headers={
+        'accept':'*/*',
+        'content-type':'application/json; charset=UTF-8',
+        // 'x-xsrf-token':document.cookie.match(/X-XSRF-TOKEN=(.+);?/)[1]
+        'x-xsrf-token':MQ.getCookieValue('X-XSRF-TOKEN')
+    };
+
+    $.ajax({
+      type: "POST",
+      data: JSON.stringify(data),
+      // dataType: '*/*',
+      headers, url, success:callback
+    });
+};
+
+MemoQ.prototype.getRows = function(callback) {
+    let o = {
+        DocInstanceId: WebTrans.Doc.docInstanceId,
+        RowIndicies: MemoQ.createArrayRange(0,this.length)
+    };
+    this.ajax('api/TranslationService/GetWebContent', o, (data)=>{
+        if(data.Success && data.Value && data.Value.Rows){
+            let rows=data.Value.Rows, res=[];
+            for(let row of rows) {
+                row=row.Row;
+                res.push({
+                    id:  row.Id,
+                    source: row.SourceSegment.EditorString,
+                    target: row.TargetSegment.EditorString
+                });
             }
-            console.log(type, e)
-            return o;
-        });
-        let url  = MQ.getAppRootUrl()+'api/TranslationService/SaveWebRows';
-        let data = { DocInstanceId:id, WebRows:rows };
-
-
-        // {"DocInstanceId":"96e71dad-9dbc-40fb-9a3c-11c42fe04811",
-        // "WebRows":[{"docInstanceId":"96e71dad-9dbc-40fb-9a3c-11c42fe04811","id":0,"sourceSegmentHtml":"[[진] 빛나는 대영주의 부츠 상자1","targetSegmentHtml":"11211","locked":false,"translationState":2,"warnings":[],"webLQAErrors":[],"comments":[],"rangeForCorrectedLQA":null,"sourceSegmentChanges":[],"targetSegmentChanges":[]}]}
-
-        let headers={
-            'accept':'*/*',
-            'content-type':'application/json; charset=UTF-8',
-            // 'x-xsrf-token':document.cookie.match(/X-XSRF-TOKEN=(.+);?/)[1]
-            'x-xsrf-token':MQ.getCookieValue('X-XSRF-TOKEN')
-        };
-
-        function success(data){
-            if(typeof callback==='function') {
-                callback(data);
-            }else{
-                console.log(data);
-            }
+            callback(res);
         }
+    });
+};
 
-        $.ajax({
-          type: "POST",
-          data: JSON.stringify(data),
-          // dataType: '*/*',
-          headers, url, success
-        });
+MemoQ.createArrayRange = function createArrayRange(start,end){
+    if(end===undefined) {
+        end=start;
+        start=0;
     }
+    let res=[];
+    while(start<=end){
+        res.push(start++);
+    }
+    return res;
+};
 
+MemoQ.prototype.setRows = function(rows, callback){
     let id=WebTrans.Doc.docInstanceId;
-    if(id){
-        saveWebRows(id,rows,function(data){
-            let success=data.Success, res;
-            if(success){
-                console.log(success,data);
+    rows=rows.map((e,i)=>{
+        let o = {
+            DocInstanceId: id,
+            id: e.id,
+            comments:[],
+            locked:false,
+            rangeForCorrectedLQA:null,
+            sourceSegmentHtml:e.source,
+            sourceSegmentChanges:[],
+            targetSegmentChanges:[],
+            targetSegmentHtml:e.target,
+            translationState:2,
+            warnings:[],
+            webLQAErrors:[]
+        };
+        return o;
+    });
+    let url  = MQ.getAppRootUrl()+'api/TranslationService/SaveWebRows';
+    let data = { DocInstanceId:id, WebRows:rows };
+
+    this.ajax('api/TranslationService/SaveWebRows', data, callback);
+};
+
+
+// test
+// mq=new MemoQ;
+// mq.getRows(e=>console.log(e));
+
+
+// mq.setRows([
+//     {id: 0, source: "[[진] 빛나는 대영주의 부츠 상자", target: "[[真]闪耀的大领主之靴宝箱1"},
+//     {id: 1, source: "[[진] 빛나는 대영주의 코트 상자", target: "[[真]闪耀的大领主大衣宝箱2"},
+// ],data=>console.log(data))
+
+
+function View(memoQ){
+    this.memoQ=memoQ;
+    let height=screen.availHeight;
+    let html=`<div id="ao-mask">
+<style>
+#ao-mask{
+    margin:0; padding:0;
+    width:100%; height:100%;
+    background:#000e;
+    position:fixed; top:0; left:0;
+    z-index:999;
+    overflow: auto;
+}
+#ao-mask table {
+    max-width:80%;
+    background:#fff;
+    margin:0 auto;
+}
+#ao-mask td{
+    min-width:1em;
+    min-height:1em;
+    vertical-align: top;
+    border: 1px solid #ccc;
+    color: #000;
+}
+#ao-mask tr:hover{
+    background:#ff07;
+}
+#ao-preview td{
+    padding: .5em;
+}
+#ao-preview tr:even {
+    background:#00f3;
+}
+#ao-mask textarea{
+    height:200px;
+    resize:none;
+}
+#ao-mask button {
+    width:4em;
+    height:2em;
+    background: #5f6;
+}
+#ao-mask #ao-edit .target {
+    user-modify: read-write-plaintext-only;
+    -webkit-user-modify: read-write-plaintext-only;
+}
+</style>
+<table>
+    <tbody id="ao-menu">
+        <tr>
+            <td></td>
+            <td>
+                <button name="close">close</button>
+            </td>
+            <td>
+                <button name="save">save</button>
+            </td>
+        </tr>
+    </tbody>
+    <tbody id="ao-edit">
+        <tr>
+            <td class="no">1</td>
+            <td class="source"><textarea readonly></textarea></td>
+            <td class="target"><textarea></textarea></td>
+        </tr>
+    </tbody>
+    <tbody id="ao-preview">
+    </tbody>
+</table>
+</div>`;
+    let c=this.content=$(html).appendTo('body');
+    c.find('button[name="close"]').one('click',()=>{
+        c.remove();
+    });
+    c.find('button[name="save"]').one('click',()=>{
+        let rows=[];
+        c.find('#ao-preview tr').each((i,e)=>{
+            e=$(e);
+            let o={
+                id: e.attr('id'),
+                source: e.find('.source').text(),
+                target: e.find('.target').text()
             }
+            if(o.target.length) rows.push(o);
+        })
+        if(rows.length){
+            this.memoQ.setRows(rows, (data)=>{
+
+                if(data.Success) {
+                    c.remove();
+                    location.reload()
+                }else{
+                    alert('원인 불명의 에러가 발생 하였습니다.');
+                    console.log(data);
+                }
+            });
+        }else{
+            alert('번역문이 전부 비어 있습니다')
+        }
+    });
+    c.find('#ao-edit .target textarea').on('input', function(e){
+        let text=e.target.value;
+        text.split('\n').forEach((t,i)=>{
+            c.find(`#ao-preview tr[id=${i}] .target`).text(t);
         });
-    }
+    })
 }
 
+// Object.defineProperty(View.prototype, 'targets', {
+//     set(v){
+//         let c=this.content;
+//         c.data('targets', v);
 
+//         let p=c.find('#ao-preview');
+//         v.split('\n').forEach((row,i)=>{
+//             p.append($('<td class="no">').text(row))
+//             p.append($('<td class="target">').text(row))
+//         })
+//     },
+//     get(){
+//         return this.content.data('targets');
+//     }
+// );
 
+// Object.defineProperty(View.prototype, 'sources', {
+//     set(v){
+//         this.content.data('sources', v);
+//     },
+//     get(){
+//         return this.content.data('sources');
+//     }
+// );
+
+// Object.defineProperty(View.prototype, 'data', {
+//     set(v){
+//         this.content.data('data', v);
+//     },
+//     get(){
+//         return this.content.data('data');
+//     }
+// );
+
+View.prototype.from=function(rows) {
+    let trs=rows.map(row=>{
+        return $('<tr>').attr('id', row.id)
+        .append($('<td class="no">').text(row.id+1))
+        .append($('<td class="source">').text(row.source))
+        .append($('<td class="target" contenteditable="read-write-plaintext-only">').text(row.target));
+    });
+
+    let sources=rows.map(row=>row.source);
+
+    this.content.find('#ao-preview').empty().append(trs);
+
+    this.content.find('#ao-edit .source textarea').val(sources.join('\n'));
+}
+
+var mq=new MemoQ;
+var v=new View(mq);
+mq.getRows(e=>{
+    v.from(e);
+});
