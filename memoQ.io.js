e0dd222378e955af4b0527ea18facff9166fffb0
ddb1494 <35029624+ddb1494@users.noreply.github.com>
2018-08-28 13:15:08 +0900

    Update memoQ.io.js

--- memoQ.io.js
+++ memoQ.io.js
@@ -227,7 +227,7 @@ View.prototype.from=function(rows) {
 
     this.content.find('#ao-preview').empty().append(trs);
 
-    this.content.find('#ao-edit .source textarea').val(sources.join('\n').replace(/[[/g,'['));
+    this.content.find('#ao-edit .source textarea').val(sources.join('\n').replace(/\[\[/g,'['));
     this.content.find('#ao-edit .target textarea').val(targets.join('\n'));
 }
 
